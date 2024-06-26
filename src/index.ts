05459ae9128fc7df3ea40d55008eea34984646c9
BlueVelocity <josephrodney@hotmail.com>
2024-06-26 01:25:10 -0400

    Build start of UI with header and inputs
    Contains framework

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
 import "./style.css";
-import GameBoard from "./model/modules/GameBoard";
-
-const board = new GameBoard();
-board.place(2, 5, 5);
-
-board.receiveAttack(5, 5);
-board.receiveAttack(5, 6);
-
-console.log(board);
-console.log(board.allSunk());
